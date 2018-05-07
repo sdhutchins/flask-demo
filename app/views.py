2b275b7113d2d416c549e4bda2df54b8b6b69591
sdhutchins <sdhutchins@outlook.com>
2018-05-06 23:35:21 -0500

    Added login, logout and contact routes.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,35 @@
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request, session, abort, flash
 
 from app import app
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template("index.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('Successful login.')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('index'))
